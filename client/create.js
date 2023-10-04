1a432c5ec6258f545ac373c967f7b51c2c436d59
unknown <ballogggamer@gmail.com>
2023-10-04 03:55:34 -0300

    implement receive

--- client/create.js
+++ client/create.js
@@ -1,5 +1,6 @@
 const net = require("net")
 const {send} = require("../tools/send")
+const {receive} = require("../tools/receive")
 
 const Connect = (ip,port) => {
     let socket = new net.Socket()
@@ -7,6 +8,7 @@ const Connect = (ip,port) => {
     socket.connect(port,ip,() => {
         console.log("\nwas connected\n")
         send(socket)
+        receive(socket)
     })
 }
 
--- server/create.js
+++ server/create.js
@@ -1,10 +1,12 @@
 const net = require("net")
 const {send} = require("../tools/send")
+const {receive} = require("../tools/receive")
 
 const createServer = (ip,port) => {
     net.createServer((socket) => {
         console.log("client was connected\n")
         send(socket)
+        receive(socket)
         
     }).listen(port,ip, () => {
         console.log("\n========================")
