13e04df6b1029b94351dc3513a41e841cb716a70
unknown <ballogggamer@gmail.com>
2023-10-04 04:14:10 -0300

    implement close

--- tools/send.js
+++ tools/send.js
@@ -1,9 +1,11 @@
 const rl = require("../readline/rl")
+const {close} = require("./close")
 
 const send = (socket) => {
     rl.question("message: ", (msg) => {
         socket.write(msg)
         send(socket)
+        close(socket,msg)
     })
 }
 
