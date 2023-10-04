e1d96bdcb8b22354234e35f26ba4b99bfb8fbc35
unknown <ballogggamer@gmail.com>
2023-10-04 03:34:32 -0300

    implement send

--- client/create.js
+++ client/create.js
@@ -1,10 +1,12 @@
 const net = require("net")
+const {send} = require("../tools/send")
 
 const Connect = (ip,port) => {
     let socket = new net.Socket()
 
     socket.connect(port,ip,() => {
-        console.log("every thing is okay")
+        console.log("\nwas connected\n")
+        send(socket)
     })
 }
 
