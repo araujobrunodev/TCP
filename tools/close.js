14319b9cb5c628df04aa4383f35ae5a179a77aa4
unknown <ballogggamer@gmail.com>
2023-10-04 04:13:48 -0300

    add close

---
+++ tools/close.js
@@ -0,0 +1,11 @@
+const rl = require("../readline/rl")
+
+const close = (socket,msg) => {
+    if (msg.match(/CLOSE/ig)) {
+        socket.destroy()
+        rl.close()
+        process.exit(0)
+    }
+}
+
+module.exports = {close}
