1806be2f0570016ab5107c45e30da5fb418d2cb9
unknown <ballogggamer@gmail.com>
2023-10-04 19:56:01 -0300

    implement listOfLines and canBreakline

--- tools/receive.js
+++ tools/receive.js
@@ -1,6 +1,14 @@
+const {canBreakline} = require("../lines/breakline")
+let {listOfLines} = require("../lines/list")
+
 const receive = (socket) => {
     socket.on("data",(data) => {
-        console.log(`\nreceived: ${data.toString("utf8")}`)
+        let line = `received: ${data.toString("utf8")}`
+        
+        if (canBreakline()) console.log(`\n${line}}`)
+        else console.log(line)
+
+        listOfLines.push(false)
     })
 }
 
--- tools/send.js
+++ tools/send.js
@@ -1,8 +1,10 @@
 const rl = require("../readline/rl")
 const {close} = require("./close")
+let {listOfLines} = require("../lines/list")
 
 const send = (socket) => {
     rl.question("message: ", (msg) => {
+        listOfLines.push(true)
         socket.write(msg)
         send(socket)
         close(socket,msg)
