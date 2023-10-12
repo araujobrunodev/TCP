ab46d2761e16d2448c925748f70b891eff52f778
unknown <ballogggamer@gmail.com>
2023-10-11 21:57:44 -0300

    implement check message empty

--- tools/send.js
+++ tools/send.js
@@ -1,14 +1,22 @@
-const {rl} = require("../readline/rl")
-const {close} = require("./close")
-let {listOfLines} = require("../lines/list")
+const { rl } = require("../readline/rl")
+const { close } = require("./close")
+let { listOfLines } = require("../lines/list")
 
 const send = (socket) => {
-    rl.question("message: ", (msg) => {
-        listOfLines.push(true)
-        socket.write(msg)
-        send(socket)
-        close(socket,msg)
-    })
+	rl.question("message: ", (msg) => {
+		switch (msg.length) {
+			case 0:
+				console.log("There's no message to be send\n")
+				break;
+
+			default:
+				socket.write(msg)
+				close(socket, msg)
+				break;
+		}
+		listOfLines.push(true)
+		send(socket)
+	})
 }
 
-module.exports = {send}
+module.exports = { send }
