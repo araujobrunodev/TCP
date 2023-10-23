e5cb28c760dc7c20e07c56f16215a5e5745f18b2
unknown <ballogggamer@gmail.com>
2023-10-23 19:46:30 -0300

    implement define IP

--- readline/defineLocalization.js
+++ readline/defineLocalization.js
@@ -1,5 +1,6 @@
-const {rl} = require("./rl")
+const { rl } = require("./rl")
 const net = require("net")
+const { GetIP } = require("./getIP")
 const { createServer } = require("../server/create")
 let local = {
     ip: "",
@@ -10,12 +11,58 @@ const defineLocalization = () => {
     console.log("=======================================")
     console.log("insert info below to create a server\n")
 
+    rl.question("Set the IP(y/n): ", (response) => {
+        switch (response) {
+            case "y":
+                defineIP()
+                break;
+
+            case "Y":
+                defineIP()
+                break;
+                
+            case "yes":
+                defineIP()
+                break;
+                
+            case "YES":
+                defineIP()
+                break;
+
+            case "n":
+                local.ip = GetIP()
+                definePort()
+                break;
+
+            case "N":
+                local.ip = GetIP()
+                definePort()
+                break;
+
+            case "no":
+                local.ip = GetIP()
+                definePort()
+                break;
+
+            case "NO":
+                local.ip = GetIP()
+                definePort()
+                break;
+                
+            default:
+                defineLocalization()
+                break;
+        }
+    })
+}
+
+function defineIP() {
     rl.question("IP: ", (localization) => {
         if (localization == "localhost" || net.isIP(localization) != 0) {
             local.ip = localization
             definePort()
-        } else 
-            return defineLocalization()
+        } else
+            return defineIP()
     })
 }
 
