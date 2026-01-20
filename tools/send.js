24bf624c8968b266c7376cb527d25da3fefd45b9
ballogg <ballogggamer@gmail.com>
2026-01-20 20:39:32 -0300

    Fix: when server fell the clients isnt warned

--- tools/send.js
+++ tools/send.js
@@ -5,31 +5,34 @@ let { clients } = require("../server/listOfClients")
 let { listOfLines } = require("../lines/list")
 
 const send = (package) => {
-	
-	listOfLines.push(true)
-	if (typeof clients == "undefined") console.log("clients is undefined");
-	rl.question("message: ", (msg) => {
-		package.msg = msg
+	try {
+		listOfLines.push(true)
+		if (typeof clients == "undefined") console.log("clients is undefined");
+		rl.question("message: ", (msg) => {
+			package.msg = msg
 
-		switch (msg.length) {
-			case 0:
-				console.log("There's no message to be send\n")
-				break;
-	
-			default:
-				if (package.isServer !== true) {
-					let socket = package.sender
-					
-					socket.write(JSON.stringify(package))
-					close(socket, msg)
-				} else {
-					broadcast(package)
-				}
-				break;
-		}
-		/* listOfLines.push(true)
-		send(something) */
-	})
+			switch (msg.length) {
+				case 0:
+					console.log("There's no message to be send\n")
+					break;
+			
+				default:
+					if (package.isServer !== true) {
+						let socket = package.sender
+
+						socket.write(JSON.stringify(package))
+						close(socket, msg)
+					} else {
+						broadcast(package)
+					}
+					break;
+			}
+			/* listOfLines.push(true)
+			send(something) */
+		})
+	} catch (err) {
+		throw err;
+	}
 }
 
 module.exports = { send }
